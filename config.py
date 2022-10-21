373303c1476b180fd099d79532a0213a0d104d98
Anurag8799 <114276044+Anurag8799@users.noreply.github.com>
2022-10-21 11:30:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,18 +12,18 @@ def is_enabled(value, default):
         return default
 
 # Mandatory variables for the bot to start
-API_ID = int(os.environ.get("API_ID")) #API ID from https://my.telegram.org/auth
-API_HASH = os.environ.get("API_HASH") #API Hash from https://my.telegram.org/auth
-BOT_TOKEN = os.environ.get("BOT_TOKEN") # Bot token from @BotFather
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split(",")] if os.environ.get("ADMINS") else []
+API_ID = int(os.environ.get("API_ID", "18238683")) #API ID from https://my.telegram.org/auth
+API_HASH = os.environ.get("API_HASH", "1068ecf4ac70a8f6ac161b7512bbf128") #API Hash from https://my.telegram.org/auth
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5701975897:AAGP4O7ir8vLHroDesq2MbtNKVx3j9wKtGI") # Bot token from @BotFather
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("5627748290")] if os.environ.get("ADMINS") else []
 
-DATABASE_NAME = os.environ.get("DATABASE_NAME")
-DATABASE_URL = os.environ.get("DATABASE_URL") # mongodb uri from https://www.mongodb.com/
-OWNER_ID =  int(os.environ.get("OWNER_ID")) # id of the owner
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Anurag789")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Anuragbot:Anuragbot@cluster0.k6jqnyf.mongodb.net/?retryWrites=true&w=majority") # mongodb uri from https://www.mongodb.com/
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5627748290")) # id of the owner
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL")) # log channel for information about users
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001891138067")) # log channel for information about users
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", False) # For Force Subscription
 BROADCAST_AS_COPY = is_enabled((os.environ.get('BROADCAST_AS_COPY', "False")), False) # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start
